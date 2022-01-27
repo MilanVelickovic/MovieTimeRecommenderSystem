1bcc8760a53a8bbda7deb7785527a9530c3e513f
Milan <mvelickovic99@gmail.com>
2022-01-27 23:42:23 +0100

    Update 1.1

--- src/graphs.py
+++ src/graphs.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 
 users = []
 genres = []
+ratings = []
 
 usersGenresDf = pd.DataFrame()
 
@@ -23,8 +24,19 @@ def loadGenres(genres):
     for genre in genres_data.get("genres"):
         genres.append(genre.get("name"))
     
+def loadRatings(ratings):
+    ratings_data = RatingAPI().getAllRatings()
+
+    for rating in ratings_data.get("ratings"):
+        user_ratings = []
+        for user_rate in rating.get("ratings"):
+            user_ratings.append(user_rate.get("userRate"))
+
+        ratings.append({"movieId": rating.get("movieId"), "ratings": user_ratings})
+
 loadUsers(users)
 loadGenres(genres)
+loadRatings(ratings)
 
 def createUsersGenresTable(users, genres, dataframe):
     for genre in genres:
@@ -39,14 +51,28 @@ def createUsersGenresTable(users, genres, dataframe):
     
 createUsersGenresTable(users, genres, usersGenresDf)
 
-def createPieChartUsersGenres(dataframe):
+def createCharts(dataframe):
     data = {}
     
     for column in dataframe.columns:
         if 1 in dataframe[column].value_counts().keys().tolist():
             data[column] = dataframe[column].value_counts()[1]
 
+    plt.subplot(1, 2, 1)
     plt.pie(data.values(), labels=data.keys(), autopct="%1.1f%%", startangle=10, wedgeprops={"edgecolor": "white"})
+    plt.title("What our users like?")
+
+    x_axis = []
+    y_axis = []
+    for rating in ratings:
+        y_axis.append(str(rating.get("movieId")))
+        x_axis.append(sum(rating.get("ratings")) / len(rating.get("ratings")))
+    
+    plt.subplot(1, 2, 2)
+    plt.barh(y_axis, x_axis)
+    plt.title("Ratings")
+    plt.xlabel("Rating")
+    plt.ylabel("Movie ID")
     plt.show()
 
-createPieChartUsersGenres(usersGenresDf)
+createCharts(usersGenresDf)
